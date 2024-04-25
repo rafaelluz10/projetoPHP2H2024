8bb0c8eafe58d2382623e2ff40966df825b6541c
Rafael Luz Tavares Pereira <rafael.luztp1@gmail.com>
2024-04-24 22:15:22 -0300

    Add files via upload

---
+++ array1.php
@@ -0,0 +1,11 @@
+<?php	
+
+$nome = array("Maria", "João", "Pedro", "Paulo", "Lucas");
+
+echo 'Os nomes no array</br>';
+
+foreach ($nome as $nomes) {
+	echo "</br>" . $nomes;
+}
+
+?>
---
+++ array2.php
@@ -0,0 +1,10 @@
+<?php
+
+$num = array(1,2,3,4,5);
+
+foreach ($num as $valor) {
+	echo "Valor é $valor<br/>";
+}
+
+
+?>
